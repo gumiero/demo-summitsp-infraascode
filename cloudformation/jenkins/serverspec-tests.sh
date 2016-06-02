88b5baec93fb39c6a396ce444d192c621849edcd
Glauber Gallego <glauber.gallego@gmail.com>
2016-06-02 02:20:05 -0300

    fixing rspec path issues + test: for 22 open

--- cloudformation/jenkins/serverspec-tests.sh
+++ cloudformation/jenkins/serverspec-tests.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 stackname="$1"
 region="$2"
 
-gem install rspec aws-sdk
+#gem install rspec aws-sdk
 
 pushd serverspec
 WebAppsStack="$(aws cloudformation describe-stacks --stack-name $stackname --region $region --output text --query 'Stacks[0].Outputs[?OutputKey==`WebAppsStack`].OutputValue')"
 export webapp_security_group="$(aws cloudformation describe-stack-resources --stack-name $WebAppsStack --region $region --query StackResources[?LogicalResourceId==\`WebAppSG\`].PhysicalResourceId --output text)"
 export webapp_region=$region
-rspec
+/usr/local/bin/rspec
 popd
