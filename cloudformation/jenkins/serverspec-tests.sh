cd5a540bf12e9ace8a042491ca7166130a3196be
Glauber Gallego <glauber.gallego@gmail.com>
2016-06-02 01:20:54 -0300

    fix rspec behaviour

--- cloudformation/jenkins/serverspec-tests.sh
+++ cloudformation/jenkins/serverspec-tests.sh
@@ -3,9 +3,10 @@
 stackname="$1"
 region="$2"
 
-WebAppsStack="$(aws cloudformation describe-stacks --stack-name $stackname --region $region --output text --query 'Stacks[0].Outputs[?OutputKey==`WebAppsStack`].OutputValue')"
+gem install rspec aws-sdk
 
 pushd serverspec
+WebAppsStack="$(aws cloudformation describe-stacks --stack-name $stackname --region $region --output text --query 'Stacks[0].Outputs[?OutputKey==`WebAppsStack`].OutputValue')"
 export webapp_security_group="$(aws cloudformation describe-stack-resources --stack-name $WebAppsStack --region $region --query StackResources[?LogicalResourceId==\`WebAppSG\`].PhysicalResourceId --output text)"
 export webapp_region=$region
 rspec
